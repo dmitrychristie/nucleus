3bc3ccaf6855845fd8de6076021cc377701a5bb0
dmitrychristie <101656568+dmitrychristie@users.noreply.github.com>
2024-03-06 15:19:56 +0100

    Update nucleus.js

--- nucleus.js
+++ nucleus.js
@@ -1,4 +1,61 @@
+!function(){
+    var analytics = window.analytics = window.analytics || [];
+    
+    if (!analytics.initialize) {
+      if (analytics.invoked) {
+        window.console && console.error && console.error("Segment snippet included twice.");
+      } else {
+        analytics.invoked = !0;
+        analytics.methods = ["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];
+        
+        analytics.factory = function(e){
+          return function(){
+            var t = Array.prototype.slice.call(arguments);
+            t.unshift(e);
+            analytics.push(t);
+            return analytics;
+          }
+        };
+        
+        for (var e = 0; e < analytics.methods.length; e++) {
+          var key = analytics.methods[e];
+          analytics[key] = analytics.factory(key);
+        }
 
+        analytics.load = function(key, e){
+          var t = document.createElement("script");
+          t.type = "text/javascript";
+          t.async = !0;
+          t.src = "https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";
+          var n = document.getElementsByTagName("script")[0];
+          n.parentNode.insertBefore(t, n);
+          analytics._loadOptions = e;
+        };
+        
+        // Function to look up the write key based on the domain name
+      function getWriteKey() {
+        var domain = window.location.hostname;
+        var writeKeys = {
+          "vendors.theknot.co": "7uNVcxnOSBpg3EinntFqHEo4Dqna4EETTTTT",
+          // Add more domain-key pairs as needed
+        };
+
+        // Check if the domain exists in the writeKeys object
+        if (writeKeys.hasOwnProperty(domain)) {
+          return writeKeys[domain];
+        } else {
+          // If the domain is not found, return a fallback key
+          return "7uNVcxnOSBpg3EinntFqHEo4Dqna4EEO";
+        }
+      }
+		
+        analytics._writeKey = getWriteKey();
+        analytics.SNIPPET_VERSION = "4.15.3";
+        analytics.load(analytics._writeKey);
+        analytics.page('Page Viewed');
+      }
+    }
+  }();
 
 
   
