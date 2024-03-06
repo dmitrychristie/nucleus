f104e6d61098bb94b9a8b1074ab2c6f20d4b6aaf
dmitrychristie <101656568+dmitrychristie@users.noreply.github.com>
2024-03-06 14:50:09 +0100

    Update nucleus.js

--- nucleus.js
+++ nucleus.js
@@ -1,61 +1,7 @@
 <script>
 
-// Base Code 
 
-!function(){
-    var analytics = window.analytics = window.analytics || [];
-    
-    if (!analytics.initialize) {
-      if (analytics.invoked) {
-        window.console && console.error && console.error("Segment snippet included twice.");
-      } else {
-        analytics.invoked = !0;
-        analytics.methods = ["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];
-        
-        analytics.factory = function(e){
-          return function(){
-            var t = Array.prototype.slice.call(arguments);
-            t.unshift(e);
-            analytics.push(t);
-            return analytics;
-          }
-        };
-        
-        for (var e = 0; e < analytics.methods.length; e++) {
-          var key = analytics.methods[e];
-          analytics[key] = analytics.factory(key);
-        }
 
-        analytics.load = function(key, e){
-          var t = document.createElement("script");
-          t.type = "text/javascript";
-          t.async = !0;
-          t.src = "https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";
-          var n = document.getElementsByTagName("script")[0];
-          n.parentNode.insertBefore(t, n);
-          analytics._loadOptions = e;
-        };
-        
-        // Function to look up the write key based on the domain name
-        function getWriteKey() {
-          var domain = window.location.hostname;
-          var writeKeys = {
-            "theknot.com": "LIVE_KEY",
-          };
-          console.log("Current domain:", domain);
-          // QA write key
-          return writeKeys[domain] || "7uNVcxnOSBpg3EinntFqHEo4Dqna4EEO";
-        }
-
-        analytics._writeKey = getWriteKey();
-        analytics.SNIPPET_VERSION = "4.15.3";
-        analytics.load(analytics._writeKey);
-        analytics.page('Page Viewed');
-      }
-    }
-  }();
-
-// END Base Code   
   
   
 // Segment Events  
