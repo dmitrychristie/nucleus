e4bf6620e18baa58dedc174255455c663b8a9a01
dmitrychristie <101656568+dmitrychristie@users.noreply.github.com>
2024-03-21 22:07:11 +0100

    Update nucleus-wordpress.js

--- nucleus-wordpress.js
+++ nucleus-wordpress.js
@@ -1,6 +1,6 @@
      function handleFormSubmission(event) {
             // Prevent form submission
-            event.preventDefault();
+           
 
             // Send a message to parent when form is submitted
             window.parent.postMessage('Form submitted from iframe!', 'https://weddingpro-com-staging.go-vip.net');
