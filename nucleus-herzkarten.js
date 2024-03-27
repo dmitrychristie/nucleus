5256e4b892da904a0008fd87abe7ad16da700dcf
dmitrychristie <101656568+dmitrychristie@users.noreply.github.com>
2024-03-27 14:43:58 +0100

    Initial commit herzkarten

--- nucleus-herzkarten.js
+++ nucleus-herzkarten.js
@@ -1,1 +1,69 @@
+!function(){
+    var analytics = window.analytics = window.analytics || [];
+    
+    if (!analytics.initialize) {
+      if (analytics.invoked) {
+        window.console && console.error && console.error("Segment snippet included twice.");
+      } else {
+        analytics.invoked = !0;
+        analytics.methods = ["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];
+        
+        analytics.factory = function(e){
+          return function(){
+            var t = Array.prototype.slice.call(arguments);
+            t.unshift(e);
+            analytics.push(t);
+            return analytics;
+          }
+        };
+        
+        for (var e = 0; e < analytics.methods.length; e++) {
+          var key = analytics.methods[e];
+          analytics[key] = analytics.factory(key);
+        }
 
+        analytics.load = function(key, e){
+          var t = document.createElement("script");
+          t.type = "text/javascript";
+          t.async = !0;
+          t.src = "https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";
+          var n = document.getElementsByTagName("script")[0];
+          n.parentNode.insertBefore(t, n);
+          analytics._loadOptions = e;
+        };
+        
+        // Function to look up the write key based on the domain name
+      function getWriteKey() {
+        var domain = window.location.hostname;
+        var writeKeys = {
+		"vendors.theknot.com": "7uNVcxnOSBpg3EinntFqHEo4Dqna4EEO",
+		"landing.hitched.co.uk": "JAx81AikCaWgsRfRFmx6RBz953eGVHqH",
+		"vendors.weddingpro.com": "3EbqDEUfCdJ1kbQ4AgVilzIGLG9LG9IC",
+	
+          // Add more domain-key pairs as needed
+        };
+	console.log(domain);
+        // Check if the domain exists in the writeKeys object
+        if (writeKeys.hasOwnProperty(domain)) {
+          return writeKeys[domain];
+        } else {
+          // If the domain is not found, return a fallback key
+          return "XeEJN55FrsKZFzBKqtu6wqnWRaZmXoKK";
+        }
+      }
+		
+        analytics._writeKey = getWriteKey();
+        analytics.SNIPPET_VERSION = "4.15.3";
+        analytics.load(analytics._writeKey);
+        analytics.page('Page Viewed');
+      }
+    }
+  }();
+
+const debugMode = getCookie('debug_mode');
+const isDebugMode = debugMode === 'true';
+
+if (isDebugMode) {
+	console.log('Debug mode enabled');
+}
+  
