5dbf20fd640f09ef9ad6a014f2d264ecb7b0857e
dmitrychristie <101656568+dmitrychristie@users.noreply.github.com>
2024-03-27 16:44:08 +0100

    Update nucleus-herzkarten.js

--- nucleus-herzkarten.js
+++ nucleus-herzkarten.js
@@ -21,24 +21,19 @@ function isConsentGranted() {
   }
 }
 
-// for now, we're only tracking anaylytics, so we need to check for other groups in the future version
-
-
-
 // Example usage
 if (isConsentGranted()) {
   console.log("Consent is granted");
-	
-!function(){
+
+  // Initialize Segment analytics only if consent is granted
+  !function(){
     var analytics = window.analytics = window.analytics || [];
-    
     if (!analytics.initialize) {
       if (analytics.invoked) {
         window.console && console.error && console.error("Segment snippet included twice.");
       } else {
         analytics.invoked = !0;
         analytics.methods = ["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];
-        
         analytics.factory = function(e){
           return function(){
             var t = Array.prototype.slice.call(arguments);
@@ -47,7 +42,6 @@ if (isConsentGranted()) {
             return analytics;
           }
         };
-        
         for (var e = 0; e < analytics.methods.length; e++) {
           var key = analytics.methods[e];
           analytics[key] = analytics.factory(key);
@@ -62,27 +56,26 @@ if (isConsentGranted()) {
           n.parentNode.insertBefore(t, n);
           analytics._loadOptions = e;
         };
-        
+
         // Function to look up the write key based on the domain name
-      function getWriteKey() {
-        var domain = window.location.hostname;
-        var writeKeys = {
-		"vendors.theknot.com": "7uNVcxnOSBpg3EinntFqHEo4Dqna4EEO",
-		"landing.hitched.co.uk": "JAx81AikCaWgsRfRFmx6RBz953eGVHqH",
-		"vendors.weddingpro.com": "3EbqDEUfCdJ1kbQ4AgVilzIGLG9LG9IC",
-	
-          // Add more domain-key pairs as needed
-        };
-	console.log(domain);
-        // Check if the domain exists in the writeKeys object
-        if (writeKeys.hasOwnProperty(domain)) {
-          return writeKeys[domain];
-        } else {
-          // If the domain is not found, return a fallback key
-          return "XeEJN55FrsKZFzBKqtu6wqnWRaZmXoKK";
+        function getWriteKey() {
+          var domain = window.location.hostname;
+          var writeKeys = {
+            "vendors.theknot.com": "7uNVcxnOSBpg3EinntFqHEo4Dqna4EEO",
+            "landing.hitched.co.uk": "JAx81AikCaWgsRfRFmx6RBz953eGVHqH",
+            "vendors.weddingpro.com": "3EbqDEUfCdJ1kbQ4AgVilzIGLG9LG9IC"
+            // Add more domain-key pairs as needed
+          };
+          console.log(domain);
+          // Check if the domain exists in the writeKeys object
+          if (writeKeys.hasOwnProperty(domain)) {
+            return writeKeys[domain];
+          } else {
+            // If the domain is not found, return a fallback key
+            return "XeEJN55FrsKZFzBKqtu6wqnWRaZmXoKK";
+          }
         }
-      }
-		
+
         analytics._writeKey = getWriteKey();
         analytics.SNIPPET_VERSION = "4.15.3";
         analytics.load(analytics._writeKey);
@@ -90,11 +83,6 @@ if (isConsentGranted()) {
       }
     }
   }();
-
-
-
-
-
 
 } else {
   console.log("Consent is not granted");
