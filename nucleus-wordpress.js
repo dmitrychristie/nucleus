b4dfda69215ca60a808fe10b39d06bbf911556e0
dmitrychristie <101656568+dmitrychristie@users.noreply.github.com>
2024-06-03 15:49:20 +0200

    Only add event listener if the form exists

--- nucleus-wordpress.js
+++ nucleus-wordpress.js
@@ -1,14 +1,15 @@
-     function handleFormSubmission(event) {
-            // Prevent form submission
-           
+function handleFormSubmission(event) {
+    // Prevent form submission
 
-            // Send a message to parent when form is submitted
-            window.parent.postMessage('Form submitted from iframe!', 'https://weddingpro-com-staging.go-vip.net');
-        }
+    // Send a message to parent when form is submitted
+    window.parent.postMessage('Form submitted from iframe!', 'https://weddingpro-com-staging.go-vip.net');
+}
 
-        // Wait for DOM content to be fully loaded
-        document.addEventListener('DOMContentLoaded', function() {
-            // Add event listener to the form for submission
-            var form = document.getElementById('pardot-form');
-            form.addEventListener('submit', handleFormSubmission);
-        });
+// Wait for DOM content to be fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+    // Add event listener to the form for submission if the form exists
+    var form = document.getElementById('pardot-form');
+    if (form) {
+        form.addEventListener('submit', handleFormSubmission);
+    }
+});
