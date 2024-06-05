bcdec74dd0a07077288aadc55917442f83cd5997
dmitrychristie <101656568+dmitrychristie@users.noreply.github.com>
2024-06-05 14:27:34 +0200

    Added GA4 properties SSOT

--- nucleus-qa.js
+++ nucleus-qa.js
@@ -2,63 +2,90 @@
     var analytics = window.analytics = window.analytics || [];
     
     if (!analytics.initialize) {
-      if (analytics.invoked) {
-        window.console && console.error && console.error("Segment snippet included twice.");
-      } else {
-        analytics.invoked = !0;
-        analytics.methods = ["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];
-        
-        analytics.factory = function(e){
-          return function(){
-            var t = Array.prototype.slice.call(arguments);
-            t.unshift(e);
-            analytics.push(t);
-            return analytics;
-          }
-        };
-        
-        for (var e = 0; e < analytics.methods.length; e++) {
-          var key = analytics.methods[e];
-          analytics[key] = analytics.factory(key);
-        }
-
-        analytics.load = function(key, e){
-          var t = document.createElement("script");
-          t.type = "text/javascript";
-          t.async = !0;
-          t.src = "https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";
-          var n = document.getElementsByTagName("script")[0];
-          n.parentNode.insertBefore(t, n);
-          analytics._loadOptions = e;
-        };
-        
-        // Function to look up the write key based on the domain name
-      function getWriteKey() {
-        var domain = window.location.hostname;
-        var writeKeys = {
-		"vendors.theknot.com": "7uNVcxnOSBpg3EinntFqHEo4Dqna4EEO",
-		"landing.hitched.co.uk": "JAx81AikCaWgsRfRFmx6RBz953eGVHqH",
-		"vendors.weddingpro.com": "3EbqDEUfCdJ1kbQ4AgVilzIGLG9LG9IC",
-	
-          // Add more domain-key pairs as needed
-        };
-	console.log(domain);
-        // Check if the domain exists in the writeKeys object
-        if (writeKeys.hasOwnProperty(domain)) {
-          return writeKeys[domain];
+        if (analytics.invoked) {
+            window.console && console.error && console.error("Segment snippet included twice.");
         } else {
-          // If the domain is not found, return a fallback key
-          return "XeEJN55FrsKZFzBKqtu6wqnWRaZmXoKK";
+            analytics.invoked = !0;
+            analytics.methods = ["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];
+            
+            analytics.factory = function(e){
+                return function(){
+                    var t = Array.prototype.slice.call(arguments);
+                    t.unshift(e);
+                    analytics.push(t);
+                    return analytics;
+                }
+            };
+            
+            for (var e = 0; e < analytics.methods.length; e++) {
+                var key = analytics.methods[e];
+                analytics[key] = analytics.factory(key);
+            }
+
+            analytics.load = function(key, e){
+                var t = document.createElement("script");
+                t.type = "text/javascript";
+                t.async = !0;
+                t.src = "https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";
+                var n = document.getElementsByTagName("script")[0];
+                n.parentNode.insertBefore(t, n);
+                analytics._loadOptions = e;
+            };
+
+            // Function to look up the write key based on the domain name
+            function getWriteKey() {
+                var domain = window.location.hostname;
+                var writeKeys = {
+                    "vendors.theknot.com": "7uNVcxnOSBpg3EinntFqHEo4Dqna4EEO",
+                    "landing.hitched.co.uk": "JAx81AikCaWgsRfRFmx6RBz953eGVHqH",
+                    "vendors.weddingpro.com": "3EbqDEUfCdJ1kbQ4AgVilzIGLG9LG9IC",
+                    // Add more domain-key pairs as needed
+                };
+                console.log(domain);
+                // Check if the domain exists in the writeKeys object
+                if (writeKeys.hasOwnProperty(domain)) {
+                    return writeKeys[domain];
+                } else {
+                    // If the domain is not found, return a fallback key
+                    return "XeEJN55FrsKZFzBKqtu6wqnWRaZmXoKK";
+                }
+            }
+            
+            // Function to get GA4 properties from cookies
+            function getGA4Properties() {
+                function getCookie(name) {
+                    let match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+                    if (match) return match[2];
+                }
+                
+                var ga4ClientId = getCookie('_ga');
+                var ga4SessionId = getCookie('_ga_s');
+                var ga4SessionNumber = getCookie('_ga_sn');
+                
+                return {
+                    ga4_client_id: ga4ClientId,
+                    ga4_session_id: ga4SessionId,
+                    ga4_session_number: ga4SessionNumber
+                };
+            }
+
+            // Add middleware to include GA4 properties
+            analytics.addSourceMiddleware(function(event, next) {
+                var ga4Properties = getGA4Properties();
+                event.context = event.context || {};
+                event.context.ga4_client_id = ga4Properties.ga4_client_id;
+                event.context.ga4_session_id = ga4Properties.ga4_session_id;
+                event.context.ga4_session_number = ga4Properties.ga4_session_number;
+                next(event);
+            });
+
+            analytics._writeKey = getWriteKey();
+            analytics.SNIPPET_VERSION = "4.15.3";
+            analytics.load(analytics._writeKey);
+            analytics.page('Page Viewed');
         }
-      }
-		
-        analytics._writeKey = getWriteKey();
-        analytics.SNIPPET_VERSION = "4.15.3";
-        analytics.load(analytics._writeKey);
-        analytics.page('Page Viewed');
-      }
     }
-  }();
+}();
 
   
 // Segment Events  
